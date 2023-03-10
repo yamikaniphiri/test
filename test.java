184b9104fec5f6ff40a104666f3eba45e192fbd2
Yamikani Phiri <yamikanidj@gmail.com>
2023-03-10 13:10:45 +0200

    init

---
+++ test.java
@@ -0,0 +1,9 @@
+class test{
+    public static void main(String[] args){
+        int length =9;
+        int width =10;
+        int area = length * width;
+        System.out.print(area);      
+              
+    }
+}
